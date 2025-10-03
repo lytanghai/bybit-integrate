012ca6cab63be6e2a74e989a1552b9d3c596b845
Tanghai Ly <tanghai.ly@wingbank.com.kh>
2025-10-03 08:41:25 +0700

    retry

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static HTML
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
